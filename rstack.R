cc91985f1fcb36087e58dbfcf744bd3b4c14bce2
Shawn O'Neil <oneilsh@gmail.com>
2014-11-19 09:35:07 -0800

    Added rstack examples

--- rstack.R
+++ rstack.R
@@ -1,5 +1,21 @@
 library(rstackdeque)
 
+## Basic Rstack insertions
+s <- rstack()
+s <- insert_top(s, "A")
+s <- insert_top(s, "B")
+s <- insert_top(s, "C")
+print(s)
+
+## peeking
+print(peek_top(s))
+
+## removal
+s <- without_top(s)
+print(s)
+
+## rstacks can hold "rows" (either as lists or 1-row DFs) to be later converted into a data frame
+## but each row has to have the same names in the same order
 rows <- rstack()
 rows <- insert_top(rows, list(name = "Bob", age = 24))
 rows <- insert_top(rows, list(name = "Joe", age = 27))
